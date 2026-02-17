e6d2f37d62e5037c2e66df362f26aff5cd795e52
Vishal Patel <5624846+vishal84@users.noreply.github.com>
2026-02-16 19:55:20 -0500

    updates

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,12 +1,11 @@
- #%%
 import os
 import asyncio
 import logging
 import vertexai
 from vertexai import agent_engines
 from vertexai.agent_engines import AdkApp
-from agent import root_agent
-from dotenv import load_dotenv, dotenv_values
+from agent.agent import root_agent
+from dotenv import load_dotenv, dotenv_values, set_key
 
 local_agent = AdkApp(
     agent=root_agent,
@@ -38,23 +37,24 @@ client = vertexai.Client(
   location=GOOGLE_CLOUD_LOCATION,
 )
 
-#%% 
 # Create on Agent Engine
 remote_app = client.agent_engines.create(
   agent=local_agent,
   config=dict(
+    agent_framework="google-adk",
     display_name=DISPLAY_NAME,
     description="A cx agent that transfer to a live chat.",
     gcs_dir_name="live-cx-agent",
-    agent_framework="google-adk",
-    extra_packages=["./agent.py"],
+    staging_bucket=STAGING_BUCKET,
+    extra_packages=["./agent"],
     requirements=[
       "google-cloud-aiplatform[adk,agent_engines]",
       "python-dotenv>=1.0.0",
       "google-cloud-storage"
     ],
-    staging_bucket=STAGING_BUCKET,
     env_vars={
+      "GOOGLE_CLOUD_AGENT_ENGINE_ENABLE_TELEMETRY": "true",
+      "OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT": "true",
       "TOPIC_ID": TOPIC_ID
     },
   ),
@@ -64,7 +64,6 @@ remote_app = client.agent_engines.create(
 # the ADK `VertexAiSessionService`.
 print(remote_app.api_resource.name.split("/")[-1])
 
-#%%
 # Test on Agent Engine
 deployed_agent = [agent.resource_name for agent in agent_engines.list(filter=f'display_name="{DISPLAY_NAME}"')]
 deployed_agent = agent_engines.get(deployed_agent[0])
@@ -78,18 +77,32 @@ async def remote_send_message(prompt: str):
       print(event)
 
 # This will work as its running in a jupyter kernel.
-await remote_send_message("What is the date and time?")
+asyncio.run(remote_send_message("What is the date and time?"))
 
-# %%
-# Update on Agent Engine
-remote_agent = [agent.resource_name for agent in agent_engines.list(filter=f'display_name="{DISPLAY_NAME}"')]
-remote_agent = agent_engines.get(remote_agent[0])
+def update_env_file(agent_engine_id, env_file_path):
+  """Updates the .env file with the agent engine ID."""
+  try:
+    set_key(env_file_path, "AGENT_ENGINE_ID", agent_engine_id)
+    print(f"Updated AGENT_ENGINE_ID in {env_file_path} to {agent_engine_id}")
+  except Exception as e:
+    print(f"Error updating .env file: {e}")
 
-remote_agent.update(
-    requirements=[
-      "google-cloud-aiplatform[adk,agent_engines]",
-      "python-dotenv>=1.0.0",
-      "google-cloud-storage"
-    ],
-    extra_packages=["./agent.py"])
-# %%
+# log remote_app
+logging.info(
+    f"Deployed agent to Vertex AI Agent Engine successfully, resource name: {remote_app.resource_name}"
+)
+
+# Update the .env file with the new Agent Engine ID
+update_env_file(remote_app.resource_name, env_path)
+# # %%
+# # Update on Agent Engine
+# remote_agent = [agent.resource_name for agent in agent_engines.list(filter=f'display_name="{DISPLAY_NAME}"')]
+# remote_agent = agent_engines.get(remote_agent[0])
+
+# remote_agent.update(
+#     requirements=[
+#       "google-cloud-aiplatform[adk,agent_engines]",
+#       "python-dotenv>=1.0.0",
+#       "google-cloud-storage"
+#     ],
+#     extra_packages=["../agent"])
